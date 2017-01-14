4c602391643c4d4147dfdc3a342015b4289084d3
tbalint19 <toth910719balint@gmail.com>
2017-01-14 10:25:41 +0100

    suited_connectors

--- player.py
+++ player.py
@@ -10,6 +10,8 @@ class Player:
             self.get_our_hand()
             if self.have_pair():
                 return 10000
+            if self.is_suited() and ((self.have_rank("A") and self.have_rank("K")) or (self.have_rank("K") and self.have_rank("Q"))):
+                return 10000
             return 0
         except:
             traceback.print_exc()
@@ -22,12 +24,11 @@ class Player:
         for player in game_state["players"]:
             if player["name"] == "Angry Piglets":
                 return player
-        0/0
 
     def get_our_hand(self):
         self.hole_cards = self.player["hole_cards"]
         self.hole_cards_ranks = "".join([card["rank"] for card in self.hole_cards])
-        self.hole_cards_ranks = "".join([card["suit"][0] for card in self.hole_cards])
+        self.hole_cards_suits = "".join([card["suit"][0] for card in self.hole_cards])
 
     def have_pair(self):
         return self.hole_cards[0]["rank"] == self.hole_cards[1]["rank"]
@@ -35,5 +36,5 @@ class Player:
     def is_suited(self):
         return self.hole_cards[0]["suit"] == self.hole_cards[1]["suit"]
 
-    def have_ace(self):
-        return self.hole_cards[0]["suit"] == "A" or self.hole_cards[1]["suit"] == "A"
+    def have_rank(self, rank):
+        return rank in self.hole_cards_ranks
